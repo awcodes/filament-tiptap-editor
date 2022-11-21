171b3aaf5fa2ce7da6c0596cab19d9ec4e536dfd
awcodes <adam@adamwgd.com>
2022-11-21 09:03:58 -0500

    catch undefined action in toolbar buttons

--- resources/views/components/button.blade.php
+++ resources/views/components/button.blade.php
@@ -9,7 +9,9 @@
     x-tooltip.raw="{{ $label }}"
     {{ $attributes }}
     class="rounded block p-1 hover:bg-gray-200 focus:bg-gray-200 dark:hover:bg-gray-800 dark:focus:bg-gray-800"
+    @if ($active)
     x-bind:class="{ 'bg-gray-300 text-gray-800 dark:bg-gray-600 dark:text-gray-300': isActive({{ $active }}, updatedAt) }"
+    @endifß
 >
     {{ $slot }}
     <x-filament-tiptap-editor::icon icon="{{ $icon }}" />
