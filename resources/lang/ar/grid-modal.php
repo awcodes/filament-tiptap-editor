ca88b0d056644d334945c53634d0946bf3d3cd39
mohamedsabil83 <me@mohamedsabil83.com>
2023-11-25 21:27:18 +0300

    Arabic translations

---
+++ resources/lang/ar/grid-modal.php
@@ -0,0 +1,14 @@
+<?php
+
+return [
+    'heading' => 'منشئ الشبكة',
+    'labels' => [
+        'submit' => 'إدراج شبكة',
+        'columns' => 'الأعمدة',
+        'stack_at' => 'تجميع عند',
+        'asymmetric' => 'غير متناسق',
+        'asymmetric_left' => 'اتساع العمود الأيسر',
+        'asymmetric_right' => 'اتساع العمود الأيمن',
+        'dont_stack' => 'منع التجميع',
+    ],
+];
