6d182e6848e48715d0eed1c57c8a274fd7a24173
zcy <3041105031@qq.com>
2023-12-05 11:24:40 +0800

    Feat: support chinese translations

---
+++ resources/lang/zh_CN/grid-modal.php
@@ -0,0 +1,14 @@
+<?php
+
+return [
+    'heading' => '网格生成器',
+    'labels' => [
+        'submit' => '插入网格',
+        'columns' => '列',
+        'stack_at' => '堆叠',
+        'asymmetric' => '不对称',
+        'asymmetric_left' => '左列跨度',
+        'asymmetric_right' => '右列跨度',
+        'dont_stack' => '不要堆叠',
+    ],
+];
