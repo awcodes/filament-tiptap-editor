a4925574433190cd61591353c2ed0e6c85753321
Anderson Prohonoski <prohonoski@outlook.com>
2023-12-27 14:36:47 -0300

    .

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -15,7 +15,7 @@ class TestCase extends Orchestra
         parent::setUp();
 
         Factory::guessFactoryNamesUsing(
-            fn (string $modelName) => 'Awcodes\\FilamentExtras\\Database\\Factories\\'.class_basename($modelName).'Factory'
+            fn (string $modelName) => 'prohonoski\\FilamentExtras\\Database\\Factories\\'.class_basename($modelName).'Factory'
         );
     }
 
