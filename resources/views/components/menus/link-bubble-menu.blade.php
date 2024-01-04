4884da142e1dbef5674873f2eb94e5284330aa61
Adam Weston <awcodes1@gmail.com>
2024-01-04 16:39:59 -0500

    Fix: bubble menu showing when not in profile for link

--- resources/views/components/menus/link-bubble-menu.blade.php
+++ resources/views/components/menus/link-bubble-menu.blade.php
@@ -3,10 +3,12 @@
     'tools' => [],
 ])
 
+@if (in_array('link', $tools))
 <div x-ref="linkBubbleMenu" class="flex gap-1 items-center" x-cloak>
     <span x-text="editor().getAttributes('link').href" class="max-w-xs truncate overflow-hidden whitespace-nowrap"></span>
     <x-filament-tiptap-editor::tools.link :state-path="$statePath" icon="edit" :active="false" label="{{ __('filament-tiptap-editor::editor.link.edit') }}"/>
     <x-filament-tiptap-editor::tools.remove-link />
 </div>
+@endif
 
 
