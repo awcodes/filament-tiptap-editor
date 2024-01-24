841c673166ef035cc9e13dbc1fd47454a12c5582
JR <contact@jeanremypraud.com>
2024-01-24 17:18:49 +0100

    add depending translation keys

--- resources/lang/en/link-modal.php
+++ resources/lang/en/link-modal.php
@@ -15,18 +15,21 @@ return [
         'url' => 'URL',
         'language' => 'Language',
         'target' => [
+            'title' => 'Target',
             'default' => 'Default',
             'new_window' => 'New Window',
             'parent' => 'Parent',
             'top' => 'Top'
         ],
         'rel' => [
+            'title' => 'Rel',
             'nofollow' => 'No Follow',
             'noopener' => 'No Opener',
             'noreferrer' => 'No Referrer',
         ],
         'as_button' => 'As Button',
         'button_theme' => [
+            'title' => 'Button theme',
             'primary' => 'Primary',
             'secondary' => 'Secondary',
             'tertiary' => 'Tertiary',
