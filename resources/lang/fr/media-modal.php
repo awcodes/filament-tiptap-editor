665dd211b4325a3408aa5f00ec9b2af5af0165bf
becker <beckermaxime84@gmail.com>
2024-02-08 17:28:20 +0100

    add lang fr

---
+++ resources/lang/fr/media-modal.php
@@ -0,0 +1,20 @@
+<?php
+
+return [
+    'heading' => [
+        'update' => 'Mettre à jour le média',
+        'insert' => 'Insérer un média',
+    ],
+    'buttons' => [
+        'cancel' => 'Annuler',
+        'insert' => 'Insérer',
+    ],
+    'labels' => [
+        'file' => 'Fichier',
+        'link_text' => 'Texte du lien',
+        'alt' => 'Texte alternatif',
+        'alt_helper_text' => 'Apprendre à décrire l\'objectif de l\'image.',
+        'title' => 'Titre',
+        'lazy' => 'Chargement différé',
+    ],
+];
