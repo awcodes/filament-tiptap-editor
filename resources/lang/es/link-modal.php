ef00ff6b05b57962ddbb3522531980b887472631
becker <beckermaxime84@gmail.com>
2024-02-09 14:49:55 +0100

    add lang es

---
+++ resources/lang/es/link-modal.php
@@ -0,0 +1,32 @@
+<?php
+
+return [
+    'heading' => [
+        'update' => 'Actualizar Enlace',
+        'insert' => 'Insertar Enlace',
+    ],
+    'buttons' => [
+        'remove' => 'Eliminar Enlace',
+        'cancel' => 'Cancelar',
+        'update' => 'Actualizar',
+        'insert' => 'Insertar',
+    ],
+    'labels' => [
+        'url' => 'URL',
+        'language' => 'Idioma',
+        'target' => [
+            'default' => 'Predeterminado',
+            'new_window' => 'Nueva Ventana',
+            'parent' => 'Principal',
+            'top' => 'Superior',
+        ],
+        'referrer_policy' => 'Política de Referencia',
+        'as_button' => 'Como Botón',
+        'button_theme' => [
+            'primary' => 'Primario',
+            'secondary' => 'Secundario',
+            'tertiary' => 'Terciario',
+            'accent' => 'Acento',
+        ],
+    ],
+];
---
+++ resources/lang/es/media-model.php
@@ -0,0 +1,20 @@
+<?php
+
+return [
+    'heading' => [
+        'update' => 'Actualizar Medios',
+        'insert' => 'Insertar Medios',
+    ],
+    'buttons' => [
+        'cancel' => 'Cancelar',
+        'insert' => 'Insertar',
+    ],
+    'labels' => [
+        'file' => 'Archivo',
+        'link_text' => 'Texto del Enlace',
+        'alt' => 'Texto Alternativo',
+        'alt_helper_text' => 'Aprende a describir el propósito de la imagen.',
+        'title' => 'Título',
+        'lazy' => 'Carga Perezosa',
+    ],
+];
