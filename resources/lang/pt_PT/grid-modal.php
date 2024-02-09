e99239fa609ed07a420f72a99d4c3a3e0f13e8e9
becker <beckermaxime84@gmail.com>
2024-02-09 15:07:17 +0100

    add lang pt_PT

---
+++ resources/lang/pt_PT/grid-modal.php
@@ -0,0 +1,14 @@
+<?php
+
+return [
+    'heading' => 'Construtor de Grade',
+    'labels' => [
+        'submit' => 'Inserir Grade',
+        'columns' => 'Colunas',
+        'stack_at' => 'Empilhar em',
+        'asymmetric' => 'Assimétrico',
+        'asymmetric_left' => 'Largura da coluna esquerda',
+        'asymmetric_right' => 'Largura da coluna direita',
+        'dont_stack' => 'Não Empilhar',
+    ],
+];
---
+++ resources/lang/pt_PT/media-modal.php
@@ -0,0 +1,19 @@
+<?php
+
+return [
+    'heading' => [
+        'update' => 'Atualizar Mídia',
+        'insert' => 'Inserir Mídia',
+    ],
+    'buttons' => [
+        'cancel' => 'Cancelar',
+        'insert' => 'Inserir',
+    ],
+    'labels' => [
+        'file' => 'Arquivo',
+        'link_text' => 'Texto do Link',
+        'alt' => 'Texto Alternativo',
+        'alt_helper_text' => 'Saiba como descrever o propósito da imagem.',
+        'title' => 'Título',
+    ],
+];
