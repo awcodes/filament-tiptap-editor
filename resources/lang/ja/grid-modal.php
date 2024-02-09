cf600c5958ccb1b8d2dbf5fda844a0388c0d4778
becker <beckermaxime84@gmail.com>
2024-02-09 15:00:57 +0100

    add lang ja

---
+++ resources/lang/ja/grid-modal.php
@@ -0,0 +1,14 @@
+<?php
+
+return [
+    'heading' => 'グリッドビルダー',
+    'labels' => [
+        'submit' => 'グリッドを挿入',
+        'columns' => 'カラム',
+        'stack_at' => 'スタック位置',
+        'asymmetric' => '非対称',
+        'asymmetric_left' => '左カラムのスパン',
+        'asymmetric_right' => '右カラムのスパン',
+        'dont_stack' => 'スタックしない',
+    ],
+];
---
+++ resources/lang/ja/oembed-modal.php
@@ -0,0 +1,25 @@
+<?php
+
+return [
+    'heading' => '動画を挿入',
+    'buttons' => [
+        'cancel' => 'キャンセル',
+        'insert' => '挿入',
+    ],
+    'labels' => [
+        'url' => 'URL',
+        'width' => '幅',
+        'height' => '高さ',
+        'responsive' => 'レスポンシブ',
+        'responsive_helper' => '動画がレスポンシブでない場合は、動画の実際のピクセル寸法に幅と高さを設定してください。デフォルトは640x480です。',
+        'autoplay' => '自動再生',
+        'loop' => 'ループ',
+        'title' => 'タイトル',
+        'byline' => 'クレジット',
+        'portrait' => 'ポートレート',
+        'controls' => 'コントロール',
+        'nocookie' => 'クッキーなし',
+        'start_at' => '開始時刻',
+        'local' => 'ローカル',
+    ],
+];
