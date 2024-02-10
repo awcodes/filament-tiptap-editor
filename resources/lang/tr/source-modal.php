6f65272058167a0e0cadd6aa1b9615a2ed0526f5
Azad Furkan ŞAKAR <afsakarr@gmail.com>
2024-02-10 22:52:56 +0300

    Add Turkish translate

---
+++ resources/lang/tr/source-modal.php
@@ -0,0 +1,12 @@
+<?php
+
+return [
+    'heading' => 'Kaynak Kodunu Düzenle',
+    'buttons' => [
+        'cancel' => 'İptal',
+        'update' => 'Güncelle',
+    ],
+    'labels' => [
+        'source' => 'Kaynak',
+    ],
+];
