414757032fb34f1645290624f9b0038b2ae44e83
mszabeh <53317316+mszabeh@users.noreply.github.com>
2024-03-13 17:04:09 +0330

    Add Fa Lang

---
+++ resources/lang/fa/source-modal.php
@@ -0,0 +1,12 @@
+<?php
+
+return [
+    'heading' => 'ویرایش کد سورس',
+    'buttons' => [
+        'cancel' => 'لغو',
+        'update' => 'به روز رسانی',
+    ],
+    'labels' => [
+        'source' => 'سورس',
+    ],
+];
