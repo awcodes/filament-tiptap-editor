9a4b40d2d5ad34184ac7ef30af08db854b8bc6eb
corean <corean@corean.biz>
2024-04-05 00:13:16 +0900

    - Korean translations add

---
+++ resources/lang/ko/oembed-modal.php
@@ -0,0 +1,25 @@
+<?php
+
+return [
+    'heading' => '비디오 삽입',
+    'buttons' => [
+        'cancel' => '취소',
+        'insert' => '삽입',
+    ],
+    'labels' => [
+        'url' => 'URL',
+        'width' => '너비',
+        'height' => '높이',
+        'responsive' => '반응형',
+        'responsive_helper' => '비디오가 반응형이 아닌 경우, 너비와 높이를 비디오의 실제 픽셀 크기로 설정하세요. 기본값은 640x480입니다.',
+        'autoplay' => '자동 재생',
+        'loop' => '반복 재생',
+        'title' => '제목',
+        'byline' => '글쓴이',
+        'portrait' => '포트레이트',
+        'controls' => '컨트롤',
+        'nocookie' => '쿠키 없음',
+        'start_at' => '시작 시간',
+        'local' => '로컬',
+    ],
+];
---
+++ resources/lang/ko/source-modal.php
@@ -0,0 +1,12 @@
+<?php
+
+return [
+    'heading' => '소스 코드 편집',
+    'buttons' => [
+        'cancel' => '취소',
+        'update' => '업데이트',
+    ],
+    'labels' => [
+        'source' => '소스',
+    ],
+];
