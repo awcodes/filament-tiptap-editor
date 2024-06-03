52da9933d33c338fffc0d268c21bb95ac968b323
alwayshopeless <38844539+alwayshopeless@users.noreply.github.com>
2024-06-03 15:09:17 +0300

    Ukrainian translation added

---
+++ resources/lang/uk/grid-modal.php
@@ -0,0 +1,14 @@
+<?php
+
+return [
+    'heading' => 'Конструктор сітки',
+    'labels' => [
+        'submit' => 'Вставити сітку',
+        'columns' => 'Колонки',
+        'stack_at' => 'Рівень складання',
+        'asymmetric' => 'Асиметрично',
+        'asymmetric_left' => 'Розмах лівої колонки',
+        'asymmetric_right' => 'Розмах правої колонки',
+        'dont_stack' => 'Не складати',
+    ],
+];
