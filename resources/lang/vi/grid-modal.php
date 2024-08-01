47a9af90bd9be8148b4dc3d0e0200f6636e3b7a3
datnt <datnt.pq23@gmail.com>
2024-08-01 14:09:54 +0700

    Translated lang file into viet nam

---
+++ resources/lang/vi/grid-modal.php
@@ -0,0 +1,14 @@
+<?php
+
+return [
+    'heading' => 'Tùy chọn về cột',
+    'labels' => [
+        'submit' => 'Thêm cột',
+        'columns' => 'Cột',
+        'stack_at' => 'Xếp chồng lại',
+        'asymmetric' => 'Đối xứng',
+        'asymmetric_left' => 'Khoảng cách bên trái',
+        'asymmetric_right' => 'Khoảng cách bên phải',
+        'dont_stack' => 'Không xếp chồng',
+    ],
+];
