db3168017cf1941495f4045183c18cfe66c03a3f
Rob Lyons <39706150+aSeriousDeveloper@users.noreply.github.com>
2024-09-06 13:33:50 +0100

    Update table-of-contents.blade.php
    Remove duplicated <ul> tags
    Reduce whitespace

--- resources/views/components/table-of-contents.blade.php
+++ resources/views/components/table-of-contents.blade.php
@@ -1,17 +1,10 @@
 <ul>
     @foreach ($headings as $heading)
-
         <li>
-
             <a href="#{{ $heading['id'] }}">{{ $heading['text'] }}</a>
-
             @if (array_key_exists('subs', $heading))
-                <ul>
-                    <x-filament-tiptap-editor::table-of-contents :headings="$heading['subs']" />
-                </ul>
+                <x-filament-tiptap-editor::table-of-contents :headings="$heading['subs']" />
             @endif
-
         </li>
-
     @endforeach
 </ul>
