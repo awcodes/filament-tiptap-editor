84e8f85aee036ee758e06f21ee3527edd13989ae
Rob Lyons <39706150+aSeriousDeveloper@users.noreply.github.com>
2024-09-09 12:51:16 +0100

    Update table-of-contents.blade.php
    use different class for list item & url

--- resources/views/components/table-of-contents.blade.php
+++ resources/views/components/table-of-contents.blade.php
@@ -5,7 +5,7 @@
 <ul class="filament-tiptap-contents-list" data-list-depth="{{$depth}}">
     @foreach ($headings as $heading)
         <li class="filament-tiptap-contents-item">
-            <a class="filament-tiptap-contents-item" href="#{{ $heading['id'] }}">{{ $heading['text'] }}</a>
+            <a class="filament-tiptap-contents-url" href="#{{ $heading['id'] }}">{{ $heading['text'] }}</a>
             @if (array_key_exists('subs', $heading))
                 <x-filament-tiptap-editor::table-of-contents :headings="$heading['subs']" :depth="$heading['depth']" />
             @endif
